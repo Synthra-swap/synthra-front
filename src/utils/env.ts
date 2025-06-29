16644cd5238e43a196f0b8924e1929db79bd19e9
jacko06v <jacomosconi@gmail.com>
2025-06-29 13:24:54 +0200

    fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,12 +1,12 @@
 export function isTestEnv(): boolean {
-  return process.env.NODE_ENV === 'test'
+  return false //process.env.NODE_ENV === 'test'
 }
 
 export function isStagingEnv(): boolean {
   // This is set in vercel builds and deploys from releases/staging.
-  return Boolean(process.env.REACT_APP_STAGING)
+  return false //Boolean(process.env.REACT_APP_STAGING)
 }
 
 export function isProductionEnv(): boolean {
-  return process.env.NODE_ENV === 'production' && !isStagingEnv()
+  return true //process.env.NODE_ENV === 'production' && !isStagingEnv()
 }
